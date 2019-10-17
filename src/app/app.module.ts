a0e43ac37fa338696aae7b9d51a61f60a3c24fd1
KaterineMoon <dkgarciad@unal.edu.co>
2019-10-17 11:35:22 -0500

    version inicial BitMusicAM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SliderComponent } from './myComponents/slider/slider.component';
+import { HeaderComponent } from './myComponents/header/header.component';
+import { FooterComponent } from './myComponents/footer/footer.component';
+import { ContentComponent } from './myComponents/content/content.component';
+import { SidebarComponent } from './myComponents/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/myComponents/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
+})
+export class FooterComponent implements OnInit {
+  /* urlFacebook: "assets/img/facebook_footer.png";
+  urlTwitter: "assets/img/twitter_footer.png";
+  urlInstagram: "assets/img/twitter_instagram.png"; */
+  constructor() {}
+
+  ngOnInit() {}
+}
